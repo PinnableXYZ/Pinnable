7872683ba80e1eac69cdcf18a8e0945bec5cd4f8
Livid <livid@v2ex.com>
2025-02-27 10:36:04 -0800

    More place to check_account to get the latest ENS/DWB balance

--- galaxy/handlers/web/profile.py
+++ galaxy/handlers/web/profile.py
@@ -3,11 +3,13 @@
 from tornado.web import authenticated
 
 from galaxy.handlers.web import WebHandler
+from galaxy.tasks.pinnable import check_account
 
 
 class ProfileHandler(WebHandler):
     @authenticated
     def get(self):
+        self.q.enqueue(check_account, self.current_user.id)
         self.finalize("profile/profile.html")
 
 
