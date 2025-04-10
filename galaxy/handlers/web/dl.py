153153630a2aa47945a9fd3e0f2e3f5d733c48b5
Livid <livid@v2ex.com>
2025-04-10 01:00:50 -0700

    Update download handlers

--- galaxy/handlers/web/dl.py
+++ galaxy/handlers/web/dl.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from galaxy.handlers.web import WebHandler
+from galaxy.handlers.web import APIHandler
 from galaxy.tasks.dl import check_update
 
 
-class DLPlanetHandler(WebHandler):
+class DLPlanetHandler(APIHandler):
     def get(self):
-        default_address = "https://github.com/Planetable/Planet/releases/download/release-0.18.3/Planet.zip"  # noqa: E501
+        default_address = "https://opensource.planetable.xyz/planet/release-0.20.2/Planet.zip"  # noqa: E501
         mc_key = "dl:planet:latest:link"
         mc_value = self.mc.get(mc_key)
         if mc_value is not None:
@@ -18,7 +18,22 @@ class DLPlanetHandler(WebHandler):
             self.redirect(default_address)
 
 
-class DLCroptopHandler(WebHandler):
+class DLPlanetInsiderHandler(APIHandler):
+    def get(self):
+        default_address = "https://opensource.planetable.xyz/planet-insider/insider-20250406-1/Planet-Insider.zip"  # noqa: E501
+        mc_key = "dl:planet-insider:latest:link"
+        mc_value = self.mc.get(mc_key)
+        if mc_value is not None:
+            self.redirect(mc_value)
+        else:
+            check_update(
+                "planet-insider",
+                "https://opensource.planetable.xyz/planet-insider/appcast.xml",
+            )  # noqa: E501
+            self.redirect(default_address)
+
+
+class DLCroptopHandler(APIHandler):
     def get(self):
         default_address = "https://opensource.planetable.xyz/croptop/croptop-20240711-1/Croptop.zip"  # noqa: E501
         mc_key = "dl:croptop:latest:link"
@@ -34,5 +49,6 @@ class DLCroptopHandler(WebHandler):
 
 dl_handlers = [
     (r"/dl/planet/?$", DLPlanetHandler),
+    (r"/dl/insider/?$", DLPlanetInsiderHandler),
     (r"/dl/croptop/?$", DLCroptopHandler),
 ]
